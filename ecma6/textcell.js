21a537352eb8b04182157955f91add51d7bae1cb
alu0100813272 <alu0100813272@ull.edu.es>
2017-03-27 10:04:04 +0000

    inicio

---
+++ ecma6/textcell.js
@@ -0,0 +1,65 @@
+ // Utils: Monkey Patching Modificar clase global extendiendola.
+String.prototype.repeat = function(times) {
+  var result = "";
+  for (var i = 0; i < times; i++)
+    result += this;
+  return result;
+}
+
+Array.prototype.range = function(block) {
+  var r = [];
+  for(var i = this[0]; i<this[1]; i++) {
+    r.push(block(i));
+  }
+  return r;
+} 
+// End Utils
+
+
+class TextCell{
+  
+// TextCell Class
+constructor(text) {
+  this.text = text.split("\n");
+}
+/*
+  minWidth() returns a number indicating this cell’s minimum width
+  (in characters).
+*/
+   minWidth(){
+  /* 
+    The reduce() method applies a function against an accumulator and
+    each element in the array (from left to right) to reduce it to a
+    single value.
+  */
+    return this.text.reduce(function(width, line) {
+      return Math.max(width, line.length);
+       }, 0);
+   };
+/*
+  minHeight() returns a number indicating the minimum height this
+  cell requires (in lines).
+*/
+  minHeight() {
+    return this.text.length;
+  };
+/*
+  draw(width, height) returns an array of length height, which contains
+  a series of strings that are each width characters wide. This
+  represents the content of the cell.
+*/
+  draw(width, height) {
+  var result = [];
+  for (var i = 0; i < height; i++) {
+    var line = this.text[i] || "";
+    result.push(line + " ".repeat(width - line.length));
+  }
+  return result; 
+  };    
+
+}
+
+
+module.exports = {
+  TextCell: TextCell
+};
